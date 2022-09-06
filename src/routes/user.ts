7514abe24ec9575e62c833e9c1edada40a8bd968
cheng chingwen <chengchingwen@chengdeMacBook-Air.local>
2022-09-07 01:10:40 +0800

    Add error arrays

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,9 +5,12 @@ import { UserRepoImpl } from './../repo/user'
 import * as TE from 'fp-ts/lib/TaskEither'
 import * as T from 'fp-ts/lib/Task'
 import * as A from 'fp-ts/lib/Array'
+import * as AP from 'fp-ts/lib/Apply'
 import * as E from 'fp-ts/lib/Either'
 import { logInfo } from '../utils/log'
 //import { userResponseSchema, usersResponseSchema, postUsersBodySchema } from '../schemas/user'
+declare function mergeFn(te: T.Task<E.Either<Error, IUser>[]>): T.Task<Results>
+type Results = { errors: Error[]; results: IUser[] };
 
 const UserRouter = (server: FastifyInstance, opts: RouteShorthandOptions, done: (error?: Error) => void) => {
     const userRepo = UserRepoImpl.of()
@@ -57,19 +60,45 @@ const UserRouter = (server: FastifyInstance, opts: RouteShorthandOptions, done: 
                     userRepo.addUser(country.country, userBody)
                 )
             )
-        //console.log("12222222", userBodys.users)
 
-
+        function mergeFn(
+            a: T.Task<E.Either<Error, IUser>[]>
+        ): T.Task<Results> {
+            return T.map((e: E.Either<Error, IUser>[]) =>
+                e.reduce(
+                    (acc, cur) => {
+                        // our reducer is still pure, as we pass fresh object literal as initial value
+                        if (E.isLeft(cur)) acc.errors.push(cur.left);
+                        if (E.isRight(cur)) acc.results.push(cur.right);
+                        return acc;
+                    },
+                    { errors: [], results: [] } as Results
+                )
+            )(a);
+        }
+        type Results = { errors: Error[]; results: IUser[] };
 
         return pipe(
             userBodys.users,
             A.map((x) => addUser(x)),
-            A.sequence(TE.MonadTask),
-            TE.match(
-                (error) => reply.status(500).send({ msg: `${error}` }),
-                (user) => reply.status(200).send({ user })
-            ),
-            T.chainFirstIOK(() => () => logInfo(`Add User`)()),
+            A.sequence(T.task), // we use sequenceT here
+            mergeFn,
+            T.map(
+                x => {
+                    if (x.errors.length > 0) reply.status(500).send({ msg: `${x.errors}` })
+                    if (x.errors.length === 0) reply.status(200).send({ users: x.results })
+                }
+            )
+            //T.chainFirstIOK((x) => () => logInfo(`${x}`)()),
+            // userBodys.users,
+            // A.map((x) => addUser(x)),
+            // A.sequence(TE.MonadTask),
+
+            // TE.match(
+            //     (error) => reply.status(500).send({ msg: `${error}` }),
+            //     (user) => reply.status(200).send({ user })
+            // ),
+            // T.chainFirstIOK(() => () => logInfo(`Add User`)()),
 
         )()
     })
